87f93fe07e93943d21c4190abe978b82568357e6
Dennis <dennistrangg@gmail.com>
2023-08-01 07:17:29 -0700

    rout tested about and home page

--- main_app/models.py
+++ main_app/models.py
@@ -7,14 +7,14 @@ from datetime import date
 # Create your models here.
 
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-    age = models.IntegerField(blank=True, null=True)
-    height = models.IntegerField(blank=True, null=True)
-    weight = models.IntegerField(blank=True, null=True)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  bio = models.TextField(max_length=500, blank=True)
+  age = models.IntegerField(blank=True, null=True)
+  height = models.IntegerField(blank=True, null=True)
+  weight = models.IntegerField(blank=True, null=True)
 
-    def __str__(self):
-        return self.user.username
-    
-    def get_absolute_url(self):
-        return reverse('profile_detail', kwargs={'pk': self.id})
+  def __str__(self):
+      return self.user.username
+  
+  def get_absolute_url(self):
+      return reverse('profile_detail', kwargs={'pk': self.id})
