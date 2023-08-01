427d0c5cb06e2a946c212e2df06b2d31a3e648a4
Samuel Baird <sam@samuelbaird.com>
2023-08-01 11:04:59 -0300

    imported user, created profile model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from datetime import date
+
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    age = models.IntegerField(blank=True, null=True)
+    height = models.IntegerField(blank=True, null=True)
+    weight = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+    
+    def get_absolute_url(self):
+        return reverse('profile_detail', kwargs={'pk': self.id})
