8bb88b330be5925fb80d68138c68a7c520042c70
Samuel Baird <sam@samuelbaird.com>
2023-08-01 16:52:17 -0300

    dynamic nav

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Profile
 
 # Create your views here.
 def home(request):
