dfa4e50e570fc8f88297c919a86b4b1cbb4bcab1
Dennis <dennistrangg@gmail.com>
2023-08-02 14:06:06 -0700

    resolved migrations

--- fitbuddy/settings.py
+++ fitbuddy/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main_app'
+    'main_app',
+    'import_export',
 ]
 
 MIDDLEWARE = [
