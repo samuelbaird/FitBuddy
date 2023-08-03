1730d30a8ccb6b1dc49b29a62f01d6db92b85ae1
corydesmond1 <corydesmond14@gmail.com>
2023-08-03 09:21:05 -0400

    added home functionality and blue exercise card

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('home/', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('workouts_index/', views.workouts_index, name='workouts_index'),
   path('exercises_index/', views.exercises_index, name='exercises_index'),
