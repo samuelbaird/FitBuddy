988378c54f2056a4c9140e6fc05d7c215af37220
Dennis <dennistrangg@gmail.com>
2023-08-03 14:16:00 -0700

    populate exercises when muscle is chosen

--- main_app/views.py
+++ main_app/views.py
@@ -57,6 +57,14 @@ def muscle_index(request, muscle):
   }
   return render(request, 'exercises/muscle_index.html', context)
 
+def muscle_exercise_detail(request, muscle, exercise_id):
+  exercise = ImportedExercise.objects.get(id=exercise_id)
+  context = {
+      'muscle_name': muscle,
+      'exercise': exercise,
+  }
+  return render(request, 'exercises/muscle_exercise_detail.html', context)
+
 @login_required
 def exercises_detail(request, exercise_id):
   exercise = Exercise.objects.get(id=exercise_id)
