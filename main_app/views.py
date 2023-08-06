735c9f154a9df6ca26ce2e6e87095801f11c173e
Samuel Baird <sam@samuelbaird.com>
2023-08-06 13:30:18 -0300

    fixed auth

--- main_app/views.py
+++ main_app/views.py
@@ -150,7 +150,6 @@ def signup(request):
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
 
-@login_required
 class ProfileUpdate(UpdateView):
     model = Profile
     # form_class = ProfileForm
@@ -170,12 +169,11 @@ class ProfileUpdate(UpdateView):
         profile.save()
         return super().form_valid(form)
 
-@login_required
 class ProfileDelete(DeleteView):
     model = Profile
     success_url = '/'
 
-@login_required
+
 class WorkoutDelete(DeleteView):
     model = Workout
     success_url = '/workouts/'
@@ -187,7 +185,7 @@ def workouts_index (request):
         'workouts': workouts
     })
 
-@login_required
+
 def workouts_detail(request, pk):
     workout = Workout.objects.get(id=pk)
     exercise_in_workouts = ExerciseInWorkout.objects.filter(workout=workout)
@@ -196,7 +194,6 @@ def workouts_detail(request, pk):
         'exercise_in_workouts': exercise_in_workouts,
     })
 
-@login_required
 def create_workout(request):
     if request.method == 'POST':
         print(request.POST)
@@ -268,7 +265,6 @@ def create_workout(request):
     exercise_list = ImportedExercise.objects.all()
     return render(request, 'workouts/workout_form.html', {'form': form, 'exercise_list': exercise_list})
 
-@login_required
 def update_workout(request, pk):
     workout = get_object_or_404(Workout, pk=pk)
 
@@ -352,6 +348,7 @@ def update_workout(request, pk):
         'workout_name': workout.name,
     })
 
+
 class ExerciseUpdate(UpdateView):
   model = Exercise
   fields = '__all__'
@@ -360,7 +357,6 @@ class ExerciseDelete(DeleteView):
   model = Exercise
   success_url = '/'  
 
-@login_required
 def begin_workout(request, pk):
     template_workout = get_object_or_404(Workout, pk=pk)
     form = WorkoutForm(instance=template_workout)
