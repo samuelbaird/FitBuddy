008e4f36a2881ebd8fc6c1d2ee47107d208d01c4
Dennis <dennistrangg@gmail.com>
2023-08-07 06:31:59 -0700

    update settings.py

--- fitbuddy/settings.py
+++ fitbuddy/settings.py
@@ -15,7 +15,7 @@ import django_heroku
 import environ
 
 
-django_heroku.settings(locals())
+
 env = environ.Env()
 environ.Env.read_env()
 
@@ -139,3 +139,5 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
